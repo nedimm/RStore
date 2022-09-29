69a0ac24d7f85590b5590784c0c140d18456efba
Nedim Mujezinovic <nedim.mujezinovic@asm-smt.com>
2022-09-29 13:33:47 +0200

    Add Serilog

--- RStore.Api/Controllers/WeatherForecastController.cs
+++ RStore.Api/Controllers/WeatherForecastController.cs
@@ -21,13 +21,24 @@ namespace RStore.Api.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            _logger.LogInformation("Made call to WeatherForecast");
+            try
             {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+                return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = Random.Shared.Next(-20, 55),
+                    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                })
+                .ToArray();
+            }
+            catch (Exception ex)
+            {
+
+                _logger.LogError(ex, "Fatal Error Occured");
+                throw;
+            }
+            
         }
     }
 }
