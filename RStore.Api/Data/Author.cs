64fdb051ed306280ae9b245c144a99441597a5a0
Nedim Mujezinovic <nedim.mujezinovic@asm-smt.com>
2022-09-29 13:39:54 +0200

    Add data classes

---
+++ RStore.Api/Data/Author.cs
@@ -0,0 +1,10 @@
+﻿namespace RStore.Api.Data;
+
+public class Author
+{
+    public int Id { get; set; }
+    public string Firstname { get; set; }
+    public string Lastname { get; set; }
+    public string Bio { get; set; }
+    public virtual IList<Book> Books { get; set; }
+}
---
+++ RStore.Api/Data/Book.cs
@@ -0,0 +1,14 @@
+﻿namespace RStore.Api.Data;
+
+public class Book
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public int? Year { get; set; }
+    public string Isbn { get; set; }
+    public string Summary { get; set; }
+    public string Image { get; set; }
+    public double? Price { get; set; }
+    public int? AuthorId { get; set; }
+    public Author Author { get; set; }
+}
