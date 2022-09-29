20a8b314d9aa46399fac7c2d8af0ef18c0321bb4
Nedim Mujezinovic <mujezinovic@gmail.com>
2022-09-29 22:09:06 +0200

    Start adding Dtos

---
+++ RStore.Api/Dto/Author/AuthorCreateDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RStore.Api.Dto.Author;
+
+public class AuthorCreateDto
+{
+    [Required]
+    [StringLength(50)]
+    public string Firstname { get; set; }
+    
+    [Required]
+    [StringLength(50)] public string Lastname { get; set; }
+
+    [StringLength(250)] 
+    public string Bio { get; set; }
+}
